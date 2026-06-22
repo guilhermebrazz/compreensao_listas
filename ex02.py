dcbbf019036cb913ed53120bf5f4dcbe8a8d6314
Guilherme Braz <7sbraz777@gmail.com>
2026-06-21 23:06:18 -0300

    coloquei os exercicios

--- ex02.py
+++ ex02.py
@@ -1,5 +1,13 @@
-'''def descendentes'''
+def descendentes(a) :
 
+  if a[1] == [] :
+
+    return []
+  
+  else :
+
+    return [ nm for nm, _ in a[1] ] +\
+           [ nm for f in a[1] for nm in descendentes(f) ]
 
 arv = ( "Maria", [ ("Joana", [ ("Lucio", []),
                                ("Jõao", [])
@@ -16,4 +24,4 @@ arv = ( "Maria", [ ("Joana", [ ("Lucio", []),
                  ]
       )
 
-print (arv)
+descendentes(arv)
